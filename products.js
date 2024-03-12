134c74143b60a9127cc6136ee888bebb62469d98
Shawn Johnson <shawnjohnson965@gmail.com>
2024-03-12 09:15:39 +0000

    javascript code for filter ver 1

--- products.js
+++ products.js
@@ -1,79 +1,50 @@
-let products = {
-    data: [
-        {
-            productName: "Acer Nitro",
-            category: "PC",
-            price: "£899",
-            image: "ACER Nitro N50-650 Gaming PC - Intel® Core™ i5, RTX 4060, 1 TB SSD 899.jpg",
-        },
-        {
-            productName: "Acer Predator",
-            category: "PC",
-            price: "£2749",
-            image: "ACER Predator Orion 7000  Gaming PC - Intel® Core™ i9, RTX 4080, 2 TB HDD & 1 TB SSD 2749.jpg",
-        },
-        {
-            productName: "PC specialist",
-            category: "PC",
-            price: "£799",
-            image: "PCSPECIALIST Icon 220 Gaming PC - AMD Ryzen 5, RTX 3050, 1 TB SSD 799.jpg",
-        },
-        {
-            productName: "Samsung",
-            category: "TV",
-            price: "£2199",
-            image: "2023 65” QN95C  Smart TV 2199.jpg",
-        },
-        {
-            productName: "JVC",
-            category: "TV",
-            price: "£179",
-            image: "JVC  Roku TV 40 Smart 179.jpg",
-        },
-        {
-            productName: "Sony Bravia",
-            category: "TV",
-            price: "£499",
-            image: "sony bravia 50 499.jpg",
-        },
-        {
-            productName: "Apple",
-            category: "Watch",
-            price: "£259",
-            image: "apple watch se 259.jpg",
-        },
-        {
-            productName: "FitBit",
-            category: "Watch",
-            price: "£44",
-            image: "fitbit inspire 2 44.jpg",
-        },
-        {
-            productName: "Samsung",
-            category: "Watch",
-            price: "£163",
-            image: "samsung watch 6 163.jpg",
-        },
-        {
-            productName: "Apple",
-            category: "Mobile",
-            price: "£1199",
-            image: "iphone 15 titanium 256 1199.jpg",
-        },
-        {
-            productName: "Google Pixel",
-            category: "Mobile",
-            price: "£1749",
-            image: "pixel fold porcelain 1749.jpg",
-        },
-        {
-            productName: "Samsung",
-            category: "Mobile",
-            price: "£1750",
-            image: "samsung z fold 5 256 1750.jpg",
-        },
-
-
-    ],
-    
+window.onload = () => {
+    filterProduct("All");
 };
+
+function filterProduct(value) {
+    console.log("Filtering products with value:", value);
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        if (value.toUpperCase() === button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    let elements = document.querySelectorAll(".pro");
+
+    elements.forEach((element) => {
+        if (value === "All" || element.classList.contains(value)) {
+            element.classList.remove("hide");
+            productsFound = true;
+        } else {
+            element.classList.add("hide");
+        }
+    });
+
+
+
+}
+
+document.getElementById("Search").addEventListener("click", () => {
+    console.log("Search button clicked");
+    let searchInput = document.getElementById("search-input").value.toUpperCase();
+    let elements = document.querySelectorAll(".description");
+    let cards = document.querySelectorAll(".pro");
+    elements.forEach((element, index) => {
+        if (element.innerText.toUpperCase().includes(searchInput)) {
+            cards[index].classList.remove("hide");
+        } else {
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+document.querySelectorAll(".button-value").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Button clicked:", button.innerText);
+        filterProduct(button.innerText);
+    });
+});
