6e4b6b7b33df529ebb94e3bdef28a35f05a98d20
hg1233 <220148717@aston.ac.uk>
2024-03-21 21:18:22 +0000

    fix about page styling
    fix inconsistent styling & php errors on about us page

--- Pages/about.php
+++ Pages/about.php
@@ -4,38 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learn More</title>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../Styles/master-style.css">
     <link rel="stylesheet" type="text/css" href="../Styles/learnmore.css">
 </head>
 <body>
     
-    <?php include_once("../PHP/header.php"); ?>
-    <h1 style="margin-top: 50px;">About Us</h1>
+    <?php include_once("../PHP/navbar.php"); ?>
 
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+        <h1 style="margin-top: 50px;">About Us</h1>
+            <div class="col-md-6 py-5">
+                
                 <div class="main">
                     <div class="service">
-                        <div class="serviec-logo">
-                            <img src="logo.jpeg" alt="">
-                        </div>
                         <h2>Our Story</h2>
                         <br>
-                        <p>Spacetech began with a vision to democratize the access to state-of-the-art technology. Founded by a team passionate about innovation, our journey started as a small initiative to bridge the gap between consumers and advanced electronics. Recognizing the challenges people faced in acquiring high-quality gadgets, we set out to create an inclusive platform offering a wide range of products to meet various demands.</p>
+                        <p>SpaceTech began with a vision to democratize the access to state-of-the-art technology.<br><br>Founded by a team passionate about innovation, our journey started as a small initiative to bridge the gap between consumers and advanced electronics.<br><br>Recognizing the challenges people faced in acquiring high-quality gadgets, we set out to create an inclusive platform offering a wide range of products to meet various demands.</p>
                     </div>
                     <div class="shadow1"></div>
                     <div class="shadow2"></div>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6 py-5">
                 <div class="main">
                     <div class="service">
-                        <div class="serviec-logo">
-                            <img src="logo.jpeg" alt="">
-                        </div>
                         <h2>Our Mission</h2>
                         <br>
-                        <p>At Spacetech, our mission is to revolutionize the accessibility of cutting-edge electronics by providing top-quality devices and accessories to all individuals, catering to their diverse needs and technological aspirations. We aim to create a seamless and reliable online platform where customers can explore, select, and purchase the latest in electronic innovation, ensuring a stellar shopping experience backed by exceptional service</p>
+                        <p>At SpaceTech, our mission is to revolutionize the accessibility of cutting-edge electronics by providing top-quality devices and accessories to all individuals, catering to their diverse needs and technological aspirations.<br><br>We aim to create a seamless and reliable online platform where customers can explore, select, and purchase the latest in electronic innovation, ensuring a stellar shopping experience backed by exceptional service.</p>
                     </div>
                     <div class="shadow1"></div>
                     <div class="shadow2"></div>
@@ -43,6 +41,7 @@
             </div>
         </div>
     </div>
+<?php include_once("../PHP/footer.php"); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-<?php include_once("./PHP/footer.php"); ?>
 </html>
